8fa3ec8e4e6baa095a01b24c96a02f32268e9a4c
Leon Windheuser <leon.windheuser@gmail.com>
2020-12-26 14:26:35 +0100

    Init project

---
+++ bird.py
@@ -0,0 +1,17 @@
+class Bird:
+    MAX_ROTATION = 25
+    ROT_VEL = 20
+
+    def __init__(self, pos_x, pos_y):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.velocity = 0
+
+    def jump(self):
+        pass
+
+    def move(self):
+        pass
+
+    def draw(self):
+        pass
---
+++ pipe.py
@@ -0,0 +1,21 @@
+class Pipe:
+    SPACE = 200
+    VELOCITY = 5
+
+    def __init__(self, pos_x):
+        self.pos_x = pos_x
+        self.top = 0
+        self.bottom = 0
+        self.passed = False
+
+    def set_height(self):
+        pass
+    
+    def collide(self):
+        pass
+
+    def move(self):
+        pass
+
+    def draw(self):
+        pass
